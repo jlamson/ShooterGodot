bef696799167f1d42a5afdacd3ff93a311474bb8
jlamson <darkmoose117@gmail.com>
2023-10-29 12:07:48 -0600

    Update laser & grenade to move in aim direction

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -24,14 +24,16 @@ func _on_gate_player_exited(body):
 	print(body)
 
 
-func _on_player_laser(pos: Vector2):
+func _on_player_laser(pos: Vector2, aim_direction: Vector2):
 	var laser = laser_scene.instantiate() as Area2D
 	laser.position = pos
+	laser.rotation = aim_direction.angle()
+	laser.direction = aim_direction
 	$Projectiles.add_child(laser)
 
 
-func _on_player_grenade(pos: Vector2):
+func _on_player_grenade(pos: Vector2, _aim_direction: Vector2):
 	var grenade = grenade_scene.instantiate() as RigidBody2D
 	grenade.position = pos
-	grenade.linear_velocity = Vector2.UP * 100
+	grenade.linear_velocity = _aim_direction * grenade.speed
 	$Projectiles.add_child(grenade)
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-signal laser(pos: Vector2)
-signal grenade(pos: Vector2)
+signal laser(pos: Vector2, aim_direction: Vector2)
+signal grenade(pos: Vector2, aim_direction: Vector2)
 
 const SPEED = 500
 const FIRE_SCALE = 1.1
@@ -17,6 +17,7 @@ func _ready():
 
 func _process(_delta):
 	handle_move()
+	handle_rotate()
 	handle_shoot()
 	handle_grenade()
 	
@@ -27,10 +28,15 @@ func handle_move():
 	move_and_slide()
 
 
+func handle_rotate():
+	look_at(get_global_mouse_position())
+	
+
 func handle_shoot():
 	if Input.is_action_pressed("primary") and can_laser:
 		var laser_spawn = $LaserStartPositions.get_children().pick_random()
-		laser.emit(laser_spawn.global_position)
+		var aim_direction = (get_global_mouse_position() - position).normalized()
+		laser.emit(laser_spawn.global_position, aim_direction)
 		
 		can_laser = false
 		$LaserTimer.start()
@@ -38,7 +44,8 @@ func handle_shoot():
 
 func handle_grenade():
 	if Input.is_action_pressed("secondary") and can_grenade:
-		grenade.emit($GrenadeStartPosition.global_position)
+		var aim_direction = (get_global_mouse_position() - position).normalized()
+		grenade.emit($GrenadeStartPosition.global_position, aim_direction)
 		
 		can_grenade = false
 		$GrenadeTimer.start()
